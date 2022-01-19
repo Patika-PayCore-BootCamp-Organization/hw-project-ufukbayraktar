03bbabb6dbaa4689c89d4b9af10482307965cb8c
ufukbayraktar <bayraktarr.ufuk@gmail.com>
2022-01-19 23:02:15 +0300

    Spring Inıt

---
+++ src/test/java/com/patika/paycore/FinalProject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.paycore.FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
